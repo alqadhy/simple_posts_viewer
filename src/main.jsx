09852ef24e9d2662ce5545297530e2b350a45f68
alqadhy <abdelrahmankhalidalqadhy@gmail.com>
2026-05-18 12:42:29 +0300

    tailwind setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+// Main CSS File
+import "./styles/index.css";
+
 // Main App Components
 import App from "./App.jsx";
 
